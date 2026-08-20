6125ef7aa668c7951e2bf9d2bc6dbc8b2607dcfb
Wongani Miyanza <wonganimiyanza@gmail.com>
2026-08-21 01:29:00 +0200

    Add week 1 lab files

---
+++ oop.java
@@ -0,0 +1,28 @@
+
+class person {
+     String name;
+    int age;
+
+    person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    void printDetails () {
+        System.out.println (name + " is " + age + " years old");
+    }
+}
+public class oop {
+    public static void main(String[] args) {
+        person[] people = {
+            new person("Ben", 10),
+            new person("Mary", 6),
+            new person("Leo", 97)
+
+        };
+
+        for (person p : people) {
+            p.printDetails();
+        }
+    }
+}
+    

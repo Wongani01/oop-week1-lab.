3a7f622d2117a9e272293f1ca2b7b9b4b20ed395
mungandairet <mungandairet@gmail.com>
2026-09-22 11:51:22 +0200

    Part 3: LinkedList ticket queue , middle insert on List and LinkedList

--- week5_collections/src/main/java/com/zut/Main.java
+++ week5_collections/src/main/java/com/zut/Main.java
@@ -1,9 +1,11 @@
 package com.zut;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        //== Part 2: ArrayList
       List<String> contacts = new ArrayList<>();
 contacts.add("Mary- 091");
 contacts.add("Jacob- 092");
@@ -14,6 +16,17 @@ contacts.remove("Fred- 094");
 System.out.println("After deleting Fred " + contacts);
 
 System.out.println(contacts);  
+//== Part 3: LinkedList
+List<String>queue = new LinkedList<>();
+queue.add("Ticket 1");
+queue.add("Ticket 3");
+System.out.println("Size: " + queue.size());
+queue.add(1, "Ticket 2");
+System.out.println(queue);
+
+//Same middle- insert on the ArrayList from Part 2
+contacts.add(1, "Alex-097 ");
+System.out.println(contacts);
         
     }
 }
